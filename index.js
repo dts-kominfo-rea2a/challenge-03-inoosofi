bac6170bf53af2dc2e9ecbba24b85f6da548b169
Ofan Sofian <ofansofian17@gmail.com>
2022-06-05 14:02:27 +0700

    feat: ini tugas 7 challenge 3 materi fungsi Ofan sofian

--- index.js
+++ index.js
@@ -21,10 +21,24 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = function (data) {
+  let arrBelanjaan = [];
+  for (let i = 0; i < data.length; i++) {
+    arrBelanjaan.push("- " + data[i].nama + " x " + data[i].kuantitas);
+  }
+  return arrBelanjaan;
+};
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = function (data) {
+  let arrTotal = [];
+  let total = null;
+  for (let i = 0; i < data.length; i++) {
+    arrTotal.push(data[i].harga * data[i].kuantitas);
+    total += arrTotal[i];
+  }
+  return total;
+};
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
